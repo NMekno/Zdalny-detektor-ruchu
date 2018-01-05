ffc1a703c36535e33bfaaa3a1da1c8fa135d32cd
SundayProgrmmer <mat357@o2.pl>
2018-01-05 09:22:40 +0100

    Added MovementDetection Arduino sketch

---
+++ MovementDetection/MovementDetection.ino
@@ -0,0 +1,29 @@
+// State change detection program
+
+// pins definition:
+const int  pirOutputPin = 2;    // PIR output pin
+
+// state variables:
+int detectionCounter = 0;    // counter for the number of move detection
+int pirOutputState = 0;      // current state of PIR output
+int lastPirState = 0;        // previous state of PIR output
+
+void setup() {
+  pinMode(pirOutputPin, INPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the pushbutton input pin:
+  pirOutputState = digitalRead(pirOutputPin);
+
+  if (pirOutputState != lastPirState) {
+    if (pirOutputState == HIGH) {
+      detectionCounter++;
+      Serial.println(detectionCounter);
+    }
+    delay(100);
+  }
+  lastPirState = pirOutputState;
+}
